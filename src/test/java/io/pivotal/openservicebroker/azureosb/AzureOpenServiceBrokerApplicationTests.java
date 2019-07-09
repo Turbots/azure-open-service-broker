609122253591d67aef37990e190584c65ff23bd0
Lars Rosenquist <lrosenquist@pivotal.io>
2019-07-09 10:27:55 +0200

    Added unit test

--- src/test/java/io/pivotal/openservicebroker/azureosb/AzureOpenServiceBrokerApplicationTests.java
+++ src/test/java/io/pivotal/openservicebroker/azureosb/AzureOpenServiceBrokerApplicationTests.java
@@ -1,16 +1,33 @@
 package io.pivotal.openservicebroker.azureosb;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.servicebroker.model.instance.CreateServiceInstanceRequest;
+import org.springframework.cloud.servicebroker.model.instance.CreateServiceInstanceResponse;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import io.pivotal.openservicebroker.azureosb.service.CosmosDBService;
+import reactor.core.publisher.Mono;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AzureOpenServiceBrokerApplicationTests {
 
+	@Autowired
+	private CosmosDBService cosmosDBService;
+
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void createServiceInstance()	{
+		CreateServiceInstanceRequest request = CreateServiceInstanceRequest.builder().build();
+		Mono<CreateServiceInstanceResponse> responseMono = cosmosDBService.createServiceInstance(request);
+		CreateServiceInstanceResponse response = responseMono.block();
+		Assertions.assertThat(response.isInstanceExisted()).isTrue();
+	}
 }
