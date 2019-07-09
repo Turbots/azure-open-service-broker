66c261eb600042433f783544a97de83f29cf4e01
Lars Rosenquist <lrosenquist@pivotal.io>
2019-07-09 11:33:20 +0200

    Added testcase for binding service.

--- src/test/java/io/pivotal/openservicebroker/azureosb/AzureOpenServiceBrokerApplicationTests.java
+++ src/test/java/io/pivotal/openservicebroker/azureosb/AzureOpenServiceBrokerApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.cloud.servicebroker.model.binding.CreateServiceInstanceBindingRequest;
 import org.springframework.cloud.servicebroker.model.instance.CreateServiceInstanceRequest;
 import org.springframework.cloud.servicebroker.model.instance.CreateServiceInstanceResponse;
 import org.springframework.http.HttpStatus;
@@ -26,33 +27,37 @@ public class AzureOpenServiceBrokerApplicationTests {
 
 	@Autowired
 	private WebTestClient webTestClient;
-	
+
 	@Autowired
 	private CosmosDBService cosmosDBService;
 
+
+	final static String INSTANCE_ID = "my-service-instance-id";
+	final static String BINDING_ID = "my-service-instance-id";
 	@Test
 	public void contextLoads() {
 	}
 
 	@Test
-	public void catalog()	{
-		webTestClient.get()
-		.uri("/v2/catalog")
-		.exchange()
-		.expectStatus().isOk();
+	public void catalog() {
+		webTestClient.get().uri("/v2/catalog").exchange().expectStatus().isOk();
 	}
 
 	@Test
-	public void createServiceInstance()	{
-		CreateServiceInstanceRequest request = CreateServiceInstanceRequest.builder()
-		.serviceDefinitionId("cosmosdb")
+	public void createServiceInstance() {
+		CreateServiceInstanceRequest request = CreateServiceInstanceRequest.builder().serviceDefinitionId("cosmosdb")
+				.planId("db-small").build();
+		webTestClient.put().uri("/v2/service_instances/{instanceId}", INSTANCE_ID)
+				.body(BodyInserters.fromObject(request)).exchange().expectStatus().isEqualTo(HttpStatus.OK);
+	}
+
+	@Test
+	public void bindServiceInstance() {
+		CreateServiceInstanceBindingRequest request = CreateServiceInstanceBindingRequest.builder().serviceDefinitionId("cosmosdb")
 		.planId("db-small")
-		.build();
-		String instanceId = "my-service-instance-id";
-		webTestClient.put()
-		.uri("/v2/service_instances/{instanceId}", instanceId)
-		.body(BodyInserters.fromObject(request))
-		.exchange()
-		.expectStatus().isEqualTo(HttpStatus.OK);
+		.serviceInstanceId(INSTANCE_ID).build();
+
+		webTestClient.put().uri("/v2/service_instances/{instanceId}/service_bindings/{bindingId}",INSTANCE_ID, BINDING_ID)
+				.body(BodyInserters.fromObject(request)).exchange().expectStatus().isEqualTo(HttpStatus.OK);
 	}
 }
